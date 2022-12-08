357babc5bceee26b6e1655add78795da5856cd6f
jack <jackdelaguilavilla03@gmail.com>
2022-12-08 18:23:17 -0500

    commit

--- config.fish
+++ config.fish
@@ -3,4 +3,5 @@ set -g -x fish_greeting 'Welcome Jack'
 if status is-interactive
     set FLINE_PATH $HOME/.config/fish/fishline
     source $FLINE_PATH/init.fish
+    source $FLINE_PATH/themes/custom.fish 
 end
